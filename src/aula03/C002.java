1a45db9c4d6e361a64fdfba40123b21b784a4c4e
Giovane Barcelos <giovanebarcelos@gmail.com>
2025-02-24 20:11:00 -0300

    feat: verificação se um número lido é par ou impar (C002)

---
+++ src/aula03/C002.java
@@ -0,0 +1,23 @@
+package aula03;
+
+import java.util.Scanner;
+
+/*
+ C002 Leia um número inteiro e informe se ele é par ou impar
+ */
+public class C002 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+
+        System.out.printf("Número: ");
+        numero = scanner.nextInt();
+
+        if (numero % 2 == 0){
+            System.out.printf("\nO número "+numero+" é par\n");
+        } else {
+            System.out.printf("\nO número "+numero+" é impar\n");
+        }
+
+    }
+}
