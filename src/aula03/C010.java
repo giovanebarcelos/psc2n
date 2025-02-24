9a92154b16f7b4216924500eb83a358ca8050e72
Giovane Barcelos <giovanebarcelos@gmail.com>
2025-02-24 20:54:12 -0300

    feat: cálculo da média final entre nota1 e nota2 e verificação de aprovação (C010)

---
+++ src/aula03/C010.java
@@ -0,0 +1,34 @@
+package aula03;
+
+import java.util.Scanner;
+
+/*
+Ler notas 1 e 2, fazer a média e verificar se ela é maior
+ou igual a 7 (aprovado) ou menor (reprovado).
+Author: Gustavo Henrique Herberts Maciel
+ */
+
+public class C010 {
+
+    public static void main (String[] args) {
+
+        // Declaração de Variáveis:
+        Scanner scanner = new Scanner(System.in);
+        float nota1, nota2, media;
+
+        // Leitura de Dados:
+        System.out.print("Digite a primeira nota: ");
+        nota1 = scanner.nextFloat();
+
+        System.out.print("Digite a segunda nota: ");
+        nota2 = scanner.nextFloat();
+
+        // Processamento:
+        media = (nota1 + nota2) / 2;
+
+        if (media >= 7){
+            System.out.print("Aprovado");}
+        else {
+            System.out.print("Reprovado");}
+    }
+}
