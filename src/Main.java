96a8ba61a2cb0c30c327a43711571c40af692aab
Giovane Barcelos <giovanebarcelos@gmail.com>
2025-02-24 19:02:25 -0300

    feat: Meu primeiro programa

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Meu Primeiro Programa!");
+
+        String nomeCompleto = "Dick Vigarista";
+        System.out.println("Nome Completo:" + nomeCompleto);
+        int idade = 56;
+        System.out.printf("O %s tem %d anos de idade\n",
+                           nomeCompleto, idade);
+
+
+
+    }
+}
