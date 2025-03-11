3cf9dffbdc585f46b3f47195c1b1e4fa6946b0a6
Giovane Barcelos <giovanebarcelos@gmail.com>
2025-03-10 21:30:09 -0300

    feat: tabuada de um número com while (R019W)

---
+++ src/aula05/R019W.java
@@ -0,0 +1,24 @@
+package aula05;
+/*
+R019 Faça um programa que calcule e mostre a tabuada
+de um número digitado pelo usuário.
+ */
+
+import java.util.Scanner;
+
+public class R019W {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Número: ");
+        int numero = scanner.nextInt();
+
+        int multiplicador = 1;
+
+        while ( multiplicador <= 10 ){
+            System.out.println(numero + " * " + multiplicador +
+                               " = " + numero * multiplicador);
+            multiplicador++;
+        }
+    }
+}
