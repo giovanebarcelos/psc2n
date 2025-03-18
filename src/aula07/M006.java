2646c99a366ac8b9c5f9c3dc0914172048604fa4
Giovane Barcelos <giovanebarcelos@gmail.com>
2025-03-17 21:32:42 -0300

    feat: verifica se uma matriz é simétrica (M006)

---
+++ src/aula07/M006.java
@@ -0,0 +1,19 @@
+package aula07;
+
+/*
+M006 Dado uma matriz quadrada de ordem 3 faça um algoritmo que verifique se a matriz é
+simétrica (aij=aji).
+ */
+public class M006 {
+    public static void main(String[] args) {
+        int[][] m1 = {{3,8,7},{8,9,12},{7,12,6}};
+        int[][] m2 = {{1,2,3},{4,5,6},{7,8,9}};
+
+        Matriz matriz = new Matriz();
+
+        System.out.println("M1 é simétrica? "+
+                            matriz.ehSimetrico(m1));
+        System.out.println("M2 é simétrica? "+
+                            matriz.ehSimetrico(m2));
+    }
+}
---
+++ src/aula07/Matriz.java
@@ -0,0 +1,15 @@
+package aula07;
+
+public class Matriz {
+    public boolean ehSimetrico(int[][] a) {
+        boolean simetrico = true;
+
+        for (int i = 0; i < a.length; i++){
+            for (int j = 0; j < a.length; j++){
+                simetrico = simetrico && a[i][j] == a[j][i];
+            }
+        }
+
+        return simetrico;
+    }
+}
